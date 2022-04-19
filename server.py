43d72891f68ae2b50d41ed3438673df7a854719b
Selenbp1 <akfkdhwk@gmail.com>
2022-04-19 21:45:12 +0900

    읽기

--- server.py
+++ server.py
@@ -7,52 +7,44 @@ app = Flask(__name__)
 topics = [
     {'id':1,  'title': 'html', 'body': 'html is ...'},
     {'id':2,  'title': 'css', 'body': 'css is ...'},
-    {'id':3,  'title': 'js', 'body': 'js is ...'},
+    {'id':3,  'title': 'js', 'body': 'js is ...'}
 ]
+
+def template(contents, content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            <h2>Welcome</h2>
+            {content}
+        </body>
+    </html>
+    '''
+
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
 
 @app.route('/')
 def index():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
-    return f'''<!doctype html>
-    <html>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                {liTags}
-            </ol>
-            <h2>Welcome</h2>
-            Hello, Web
-        </body>
-    </html>
-    '''
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
 
 @app.route('/read/<int:id>/')
 def read(id):
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
     title = ''
     body = ''
     for topic in topics:
-            if id == topic['id']:
-                title = topic['title']
-                body = topic['body']
-                break
-    print(title,body)
-    return f'''<!doctype html>
-    <html>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                {liTags}
-            </ol>
-            <h2>({title}</h2>
-            {body}
-        </body>
-    </html>
-    '''
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents, f'<h2>{title}</h2>{body}')
 
 @app.route('/create/')
 def create():
