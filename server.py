a79ac27d7792c4befc562ea2ca03520ee5017806
Selenbp1 <akfkdhwk@gmail.com>
2022-04-19 21:30:35 +0900

    홈페이지 구현

--- server.py
+++ server.py
@@ -1,20 +1,64 @@
+from re import L
 from flask import Flask
 import random
 
 app = Flask(__name__)
 
+topics = [
+    {'id':1,  'title': 'html', 'body': 'html is ...'},
+    {'id':2,  'title': 'css', 'body': 'css is ...'},
+    {'id':3,  'title': 'js', 'body': 'js is ...'},
+]
+
 @app.route('/')
 def index():
-    return 'random : <strong>'+str(random.random())+'</strong>'
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>
+    '''
+
+@app.route('/read/<int:id>/')
+def read(id):
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    title = ''
+    body = ''
+    for topic in topics:
+            if id == topic['id']:
+                title = topic['title']
+                body = topic['body']
+                break
+    print(title,body)
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>({title}</h2>
+            {body}
+        </body>
+    </html>
+    '''
 
 @app.route('/create/')
 def create():
     return 'Create'
 
-@app.route('/read/<id>/')
-def read(id):
-    print(id)
-    return 'Read '+ id
+
 
 
 app.run(port=5001, debug=True)
